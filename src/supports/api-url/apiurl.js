e4aecb42c680be9234aff746e3aa01fb3251eeec
andresir <bisnis.ir@gmail.com>
2019-01-05 10:52:34 +0700

    modif port

--- src/supports/api-url/apiurl.js
+++ src/supports/api-url/apiurl.js
@@ -1,2 +1,2 @@
-export const API_URL_1 = "http://localhost:1997";
+export const API_URL_1 = "http://localhost:1998";
 // export const API_URL_1 = "https://my-json-server.typicode.com/andresir/fakeAPI";
